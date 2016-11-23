cb12c1c6697497afa632660f5e6a3bd8ba835e99
Matt Gaikema <mgaikema1@gmail.com>
2016-11-23 16:59:50 -0600

    Convert homework to jinja2

--- homework/homework.py
+++ homework/homework.py
@@ -1,5 +1,7 @@
 import sys
 import os
+import jinja2
+from jinja2 import Template
 
 def create_project(assignment, folder):
 #{
@@ -7,12 +9,34 @@ def create_project(assignment, folder):
 	# http://stackoverflow.com/a/5137509/5415895
 	this_path = os.path.dirname(os.path.realpath(__file__))
 
+	env = jinja2.Environment(
+		block_start_string = '\BLOCK{',
+		block_end_string = '}',
+		variable_start_string = '\VAR{',
+		variable_end_string = '}',
+		line_statement_prefix = "-%",
+		trim_blocks = True,
+		autoescape = False,
+   		loader = jinja2.FileSystemLoader(this_path)
+	)
+
 	# Check if the folder exists, and create it if necessary.
 	# http://stackoverflow.com/a/273227/5415895
 	if not os.path.exists(folder):
 		os.makedirs(folder)
 
-	os.system("m4 -DASSIGNMENT='" +  assignment + "' " + this_path + "/main.tex.m4 >" + folder + "/main.tex")
-	os.system("m4 " + this_path + "/Makefile.m4 > " + folder + "/Makefile")
-	os.system("m4 -DASSIGNMENT='" +  assignment + "' " + this_path + "/README.md.m4 >" + folder + "/README.md")
+	tex_template = env.get_template('main.tex')
+	tex_string = tex_template.render(assignment = assignment)
+	with open(folder + "/main.tex", "w+") as f:
+  		f.write(tex_string)
+	
+	make_template = env.get_template('Makefile')
+	make_string = make_template.render()
+	with open(folder + "/Makefile", "w+") as f:
+  		f.write(make_string)
+
+	readme_template = env.get_template('README.md')
+	readme_string = readme_template.render(assignment = assignment)
+	with open(folder + "/README.md", "w+") as f:
+  		f.write(readme_string)
 #}
