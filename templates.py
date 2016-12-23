c2c7fa9f77549d20f0e4df4c0d3459fa21342af8
Matt Gaikema <mgaikema1@gmail.com>
2016-12-23 17:53:22 -0600

    Use argparser

--- templates.py
+++ templates.py
@@ -1,12 +1,29 @@
 import sys
 import os
+import argparse
 import homework.homework as homework
 import cpp.cpp as cpp
 import typescript.typescript as ts
 
-if sys.argv[1] == "homework":
-	homework.create_project(sys.argv[2], sys.argv[3])
-elif sys.argv[1] == "cpp":
-	cpp.create_project(sys.argv[2], sys.argv[3])
-elif sys.argv[1] == "ts":
-	ts.create_project(sys.argv[2], sys.argv[3])
+parser = argparse.ArgumentParser(description="Create an empty project.")
+subparsers = parser.add_subparsers(help='type of project.', dest='type')
+parser.add_argument('-d', "--dir", help="The directory where the project should be.", required=True)
+parser.add_argument('-t', '--title', help="Title of the project being made", required=True)
+
+# C++ subparser.
+cpp_parser = subparsers.add_parser('cpp', help='Make a C++ project')
+
+# Homework subparser.
+hw_subparser = subparsers.add_parser('homework', help="Make a homework document")
+
+# Typescript subparser.
+ts_subparser = subparsers.add_parser('ts', help="Make a homework document")
+
+args = vars(parser.parse_args())
+
+if args['type'] == "cpp":
+	cpp.create_project(args['title'], args['dir'])
+elif args['type'] == "homework":
+	homework.create_project(args['title'], args['dir'])
+elif args['type'] == 'ts':
+	ts.create_project(args['title'], args['dir'])
