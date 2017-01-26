537d639350dc593154a606028e5ee558ab8fcf94
Matt Gaikema <mgaikema1@gmail.com>
2017-01-26 12:58:16 -0600

    build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+# "Compile" the project into a zip archive, 
+# which can apparently be executed by python.
+# http://blog.ablepear.com/2012/10/bundling-python-files-into-stand-alone.html
+
+# Zip everything.
+zip -r templates.zip *
+# Insert shebang in zip.
+echo '#!/usr/bin/env python' | cat - templates.zip > templates
+# Change access.
+chmod +x templates
