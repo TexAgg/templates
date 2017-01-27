cd96f0def21a4c3e06c6f7dc9003ee9ae3470a0b
Matt Gaikema <mgaikema1@gmail.com>
2017-01-26 21:28:33 -0600

    add executable to path

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 # "Compile" the project into a zip archive, 
 # which can apparently be executed by python.
 # http://blog.ablepear.com/2012/10/bundling-python-files-into-stand-alone.html
+#
+# The executable doesn't work.
+# Need pkg_resources
 
 # Zip everything.
 zip -r templates.zip *
@@ -8,3 +11,5 @@ zip -r templates.zip *
 echo '#!/usr/bin/env python' | cat - templates.zip > templates
 # Change access.
 chmod +x templates
+# Copy executable to path.
+cp templates /usr/local/bin
