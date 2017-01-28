201c87660051696d4182b1b68bbc2f4c6e73e2fb
Matt Gaikema <mgaikema1@gmail.com>
2017-01-28 14:02:31 -0600

    typescript templates

---
+++ typescript/build.py
@@ -0,0 +1,44 @@
+# Reads the files in tsconfig.json, compiles them to javascript,
+# and then browserifies and uglifies the javascript.
+# The newest version of this file can be found at <https://bitbucket.org/snippets/gaikema/zoMq8/>.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import json
+import os
+import glob
+
+# Read the tsconfig to get the input files.
+with open('tsconfig.json') as data_file:    
+    data = json.load(data_file)
+ts_files = data['files']
+
+# Compile typescript files.
+print("Compiling typescript files")
+os.system("tsc")
+
+# http://stackoverflow.com/a/3964691/5415895
+os.chdir("build")
+js_files = glob.glob("*.js")
+# Make dist subdirectory if not made yet.
+# http://stackoverflow.com/a/273227/5415895
+if not os.path.exists("dist"):
+    os.makedirs("dist")
+print("Browserifying files")
+os.system("browserify " + " ".join(js_files) + " -o dist/bundle.js")
+
+print("Compressing files")
+os.system("uglifyjs dist/bundle.js -c -m -o dist/bundle.min.js")
+
+print("Done")
--- typescript/typescript.py
+++ typescript/typescript.py
@@ -1,11 +1,25 @@
 import sys
 import os
+import jinja2
+from jinja2 import Template
+import pkg_resources
 
 def create_project(project, folder):
 #{
 	# Get name for this path.
 	# http://stackoverflow.com/a/5137509/5415895
 	this_path = os.path.dirname(os.path.realpath(__file__))
+
+	env = jinja2.Environment(
+		block_start_string = '\BLOCK{',
+		block_end_string = '}',
+		variable_start_string = '\VAR{',
+		variable_end_string = '}',
+		line_statement_prefix = "-%",
+		trim_blocks = True,
+		autoescape = False,
+   		loader = jinja2.FileSystemLoader(this_path)
+	)
 
 	# Check if the folder exists, and create it if necessary.
 	# http://stackoverflow.com/a/273227/5415895
@@ -14,8 +28,23 @@ def create_project(project, folder):
 	if not os.path.exists(folder + "/scripts"):
 		os.mkdir(folder + "/scripts")
 
-	os.system("m4 " + this_path + "/tsconfig.json.m4 > " + folder + "/tsconfig.json")
-	os.system("m4 " + this_path + "/main.ts.m4 > " + folder + "/scripts/main.ts")
-	os.system("m4 " + this_path + "/build.py.m4 > " + folder + "/build.py")
-	os.system("m4 -DPROJECT='" +  project + "' " + this_path + "/README.md.m4 >" + folder + "/README.md")
+	tsconfig_template = env.from_string(pkg_resources.resource_string(__name__, 'tsconfig.json'))
+	tsconfig_string = tsconfig_template.render()
+	with open(folder + "/tsconfig.json", "w+") as f:
+		f.write(tsconfig_string)
+
+	main_template = env.from_string(pkg_resources.resource_string(__name__, 'main.ts'))
+	main_string = main_template.render()
+	with open(folder + "/main.ts", "w+") as f:
+		f.write(main_string)
+
+	build_template = env.from_string(pkg_resources.resource_string(__name__, 'build.py'))
+	build_string = build_template.render()
+	with open(folder + "/build.py", "w+") as f:
+		f.write(build_string)
+
+	readme_template = env.from_string(pkg_resources.resource_string(__name__, 'README.md'))
+	readme_string = readme_template.render(project = project)
+	with open(folder + "/README.md", "w+") as f:
+		f.write(readme_string)
 #}
