98f469f9c7e38296f4c273d7a95359a09831943e
Matt Gaikema <mgaikema1@gmail.com>
2017-04-03 17:36:46 -0500

    clean ts

--- src/typescript/typescript.py
+++ src/typescript/typescript.py
@@ -1,8 +1,5 @@
 import sys
 import os
-import jinja2
-from jinja2 import Template
-import pkg_resources
 from ..render import Renderer
 
 def create_project(project, folder):
@@ -10,17 +7,6 @@ def create_project(project, folder):
 	# Get name for this path.
 	# http://stackoverflow.com/a/5137509/5415895
 	this_path = os.path.dirname(os.path.realpath(__file__))
-
-	env = jinja2.Environment(
-		block_start_string = '\BLOCK{',
-		block_end_string = '}',
-		variable_start_string = '\VAR{',
-		variable_end_string = '}',
-		line_statement_prefix = "-%",
-		trim_blocks = True,
-		autoescape = False,
-   		loader = jinja2.FileSystemLoader(this_path)
-	)
 
 	# Check if the folder exists, and create it if necessary.
 	# http://stackoverflow.com/a/273227/5415895
