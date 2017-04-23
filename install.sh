6683b84216bf7f513d7be65c731f6ff57cca3e7d
Matt Gaikema <mgaikema1@protonmail.com>
2017-04-23 12:56:55 -0500

    fix infinite zip

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 # which can apparently be executed by python.
 # http://blog.ablepear.com/2012/10/bundling-python-files-into-stand-alone.html
 
-# Zip everything.
-zip -r templates.zip *
+# Zip required files: http://bit.ly/2pT4J8W.
+zip -r templates.zip * -x *.pyc .git\* resources.md templates debian\* test\*
 # Insert shebang in zip.
 echo '#!/usr/bin/env python' | cat - templates.zip > templates
 # Change access.
