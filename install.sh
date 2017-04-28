c6fb721c12e4afdce49f43f4cf97798d5ced8594
Matt Gaikema <mgaikema1@protonmail.com>
2017-04-27 19:33:29 -0500

    small error

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # http://blog.ablepear.com/2012/10/bundling-python-files-into-stand-alone.html
 
 # Zip required files: http://bit.ly/2pT4J8W.
-zip -r templates.zip * -x *.pyc .git\* resources.md templates debian\* test\*
+zip -r templates.zip * -x *.pyc .git\* resources.md templates debian\* test\* *.deb
 # Insert shebang in zip.
 echo '#!/usr/bin/env python' | cat - templates.zip > templates
 # Change access.
