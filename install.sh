5fe4dc331286721871a6fb1dca2a148b1b1df8c2
Matt Gaikema <mgaikema1@protonmail.com>
2017-05-07 15:40:42 -0500

    package metadata

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # "Compile" the project into a zip archive, 
 # which can apparently be executed by python.
-# http://blog.ablepear.com/2012/10/bundling-python-files-into-stand-alone.html
+# http://bit.ly/2pUwe4m
 
 # Zip required files: http://bit.ly/2pT4J8W.
 zip -r templates.zip * -x *.pyc .git\* resources.md templates debian\* test\* *.deb
@@ -15,13 +15,13 @@ rm templates.zip
 
 # BETA: create deb package.
 
-$TEMP=temp.sh
+TEMP=temp.sh
 echo "sudo mv templates /usr/local/bin" > $TEMP
 
-# https://github.com/jordansissel/fpm/wiki
+# http://bit.ly/2pQlOlJ
 ## http://stackoverflow.com/a/1955555/5415895
 VERSION=$(python -c "import sys, json; print json.load(sys.stdin)['version']" < package.json)
 ## http://stackoverflow.com/a/37124240/5415895
-fpm -s dir -t deb -n "templates" --after-install $TEMP -d python -v $VERSION templates
+fpm -s dir -t deb -n "templates" --after-install $TEMP -d python -d jinja2 -v $VERSION --description "Templates and scripts for easily creating projects" --url "https://github.com/TexAgg/templates" --maintainer "mgaikema1@protonmail.com" --license "GPL-3.0" templates
 
 rm $TEMP
