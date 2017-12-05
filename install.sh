c643cdf12b6088767f9caa4466aff2e61bf3e3d3
Matt Gaikema <mgaikema1@protonmail.com>
2017-12-05 14:03:51 -0600

    add python-jinja dependency

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # http://bit.ly/2pUwe4m
 
 # Zip required files: http://bit.ly/2pT4J8W.
-zip -r templates.zip * -x *.pyc .git\* resources.md templates debian\* test\* *.deb
+zip -r templates.zip * -x *.pyc .git\* resources.md templates debian\* test\* *.deb .vscode\*
 # Insert shebang in zip.
 echo '#!/usr/bin/env python' | cat - templates.zip > templates
 # Change access.
@@ -14,14 +14,15 @@ sudo cp templates /usr/local/bin
 rm templates.zip
 
 # BETA: create deb package.
+# This will not install it.
 
 TEMP=temp.sh
 echo "sudo mv templates /usr/local/bin" > $TEMP
 
-# http://bit.ly/2pQlOlJ
-## http://bit.ly/2wLYWrT
+# FPM: http://bit.ly/2pQlOlJ
+## Parse version field from project.json: http://bit.ly/2wLYWrT
 VERSION=$(python -c "import sys, json; print json.load(sys.stdin)['version']" < package.json)
 ## http://bit.ly/2vuLn0q
-fpm -s dir -t deb -n "templates" --after-install $TEMP -d python -d jinja2 -v $VERSION --description "Templates and scripts for easily creating projects" --url "https://github.com/TexAgg/templates" --maintainer "mgaikema1@protonmail.com" --license "GPL-3.0" templates
+fpm -s dir -t deb -n "templates" --after-install $TEMP -d python -d python-jinja2 -v $VERSION --description "Templates and scripts for easily creating projects" --url "https://github.com/TexAgg/templates" --maintainer "mgaikema1@protonmail.com" --license "GPL-3.0" templates
 
 rm $TEMP
